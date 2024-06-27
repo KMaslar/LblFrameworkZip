240b16269d0c22a6aee7f1c48f5345862ba1dd54
Kostadina Geceska <kostadinag@nextsense.com>
2024-06-27 11:49:28 +0200

    add framework

---
+++ LblFramework.xcframework/ios-arm64/LblFramework.framework/Headers/LblFramework.h
@@ -0,0 +1,18 @@
+//
+//  LblFramework.h
+//  LblFramework
+//
+//  Created by Kostadina Gecevska on 27.6.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LblFramework.
+FOUNDATION_EXPORT double LblFrameworkVersionNumber;
+
+//! Project version string for LblFramework.
+FOUNDATION_EXPORT const unsigned char LblFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LblFramework/PublicHeader.h>
+
+
